fb93b7feb1b338424b97f779ab3244da0ac9dd66
illocarlos <carlosalbendiz93@gmail.com>
2024-05-05 16:37:32 +0200

    voy a introducir desde aqui el login y signup ya qe tengo que tocar el archivo appjs y como aun no habeis mergeado lo introduzco y lo teneis todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import NavBar from "./components/NavBar/NavBar"
-import AppRoutes from './routes/AppRoutes'
-// import Index from "./pages/Index/Index"
+import { useLocation } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import AppRoutes from './routes/AppRoutes';
+import { useEffect, useMemo } from "react";
 
 function App() {
+  // te da la ruta del parametro en el que te encuentras
+  const location = useLocation();
+
+  // para desaparezca la navbar en logIn y SignUP
+  const isLogIndOrSignUp = useMemo(() => location.pathname === '/auth/LogIn' || location.pathname === '/auth/SignUp', [location])
 
   return (
     <>
-      <NavBar />
-      <AppRoutes />
+      {/* esta clase la colocaremos en _base */}
+      <div className={isLogIndOrSignUp ? 'nav-bar-hidden' : 'nav-bar-block'}>
+        <NavBar />
+      </div>
 
+      <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import PrivateRoute from './PrivateRoutes'
 import Index from '../pages/Index/Index'
+import LogInPage from '../pages/AuthPage/LoginPage/LoginPage'
+
 
 
 
@@ -9,8 +11,10 @@ const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path='/' element={<Index />} />
+            <Route path={'/'} element={<Index />} />
+            <Route path={'/auth/LogIn'} element={<LogInPage />} />
             <Route element={<PrivateRoute />}>
+
             </Route>
             <Route path='*' element={<h1> NOTHING BUT YOU ARE INSIDE </h1>} />
         </Routes>
