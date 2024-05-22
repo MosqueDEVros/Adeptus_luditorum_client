1963d86b785c337415cf9feb711569cab1c91d37
nachosanson6 <nachosanson6@gmail.com>
2024-05-22 12:12:29 +0200

    about page and css

--- src/pages/AboutPage/AboutPAge.jsx
+++ src/pages/AboutPage/AboutPAge.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 
 const AboutPAge = () => {
     return (
-        <div className="about_frame">
-            <div id='About' className='aboutPage'>
-                <div className="tittle_frame">
-                    <h1 className='tittle'>¿Quienes somos?</h1>
-                    <div className="line"></div>
+        <div className="about_page">
+            <div id='About' className='about_frame'>
+                <div className="background"></div>
+                <div className="about_content">
+                    <div className="text_frame">
+                        <div className="tittle_frame">
+                            <h1 className='tittle'>¿Quienes somos?</h1>
+                            <div className="line"></div>
+                        </div>
+                        <p className='info'>Desde 2014, en <span className='subtittle'> Adeptus Luditorum </span>Tenerife nos dedicamos a fomentar una comunidad de juegos en un ambienta acogedor y vibrante.
+                            En el año 2023, obtuvimos el reconocimiento oficial como asociación.
+                            Nuestra comunidad cuenta con más de 50 personas apasionadas por una variedad de juegos de estrategia, miniaturas, juegos de mesa y rol.
+                            Además, contamos con múltiples mesas especialmente preparadas para el <span>disfrute comunitario</span>, ya sea para lanzar dados y mover figuras o para realizar proyectos creativos en nuestras áreas de trabajo.
+                            En nuestras instalaciones también disponemos de una cafetería donde puedes disfrutar de una amplia variedad de consumibles, para hacer de tu estancia una experiencia aún más placentera.
+                            ¿Estás buscando un <span>lugar donde jugar</span> o simplemente quieres <span>conectar con otros jugadores</span> que compartan los mismo intereses?
+                            ¡No dudes en unirte! Estamos aquí para ayudarte a disfrutar al máximo de tu pasión por los juegos.</p>
+                    </div>
+                    <div className="ilustration">
+                    </div>
                 </div>
-                <p className='info'>Desde 2014, en <span className='subtittle'> Adeptus Luditorum </span>Tenerife nos dedicamos a fomentar una comunidad de juegos en un ambienta acogedor y vibrante.
-                    En el año 2023, obtuvimos el reconocimiento oficial como asociación.
-                    Nuestra comunidad cuenta con más de 50 personas apasionadas por una variedad de juegos de estrategia, miniaturas, juegos de mesa y rol.
-                    Además, contamos con múltiples mesas especialmente preparadas para el <span>disfrute comunitario</span>, ya sea para lanzar dados y mover figuras o para realizar proyectos creativos en nuestras áreas de trabajo.
-                    En nuestras instalaciones también disponemos de una cafetería donde puedes disfrutar de una amplia variedad de consumibles, para hacer de tu estancia una experiencia aún más placentera.
-                    ¿Estás buscando un <span>lugar donde jugar</span> o simplemente quieres <span>conectar con otros jugadores</span> que compartan los mismo intereses?
-                    ¡No dudes en unirte! Estamos aquí para ayudarte a disfrutar al máximo de tu pasión por los juegos.</p>
             </div>
         </div>
     )
