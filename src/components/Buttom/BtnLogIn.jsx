e7dd916d48d0f4018a9e18571204b96b93668ffc
illocarlos <carlosalbendiz93@gmail.com>
2024-05-31 20:35:12 +0200

    joan y yo implementamos formkit un paquete npm y otro de iconos aparte de tocar las validaciones de estos

--- src/components/Buttom/BtnLogIn.jsx
+++ src/components/Buttom/BtnLogIn.jsx
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom"
+import { useMemo } from "react";
+import { Link, useLocation } from "react-router-dom"
 
 
 const BtnLogIn = () => {
+
+    const location = useLocation();
+    console.log(location.pathname)
+    const isRouteLoginOrSignUp = useMemo(() => location.pathname === '/auth/LogIn', [location])
+
     return (
         <div className="block-logIn-SignUp">
 
-            <Link to={`/auth/LogIn`} className="no-decoration">
-                <div className="left-block">Ingresar</div>
+            <Link to={`/auth/LogIn`} className={`${isRouteLoginOrSignUp ? 'activeShadowlogIn' : 'no-decoration'}`}>
+                <div className={`left-block ${isRouteLoginOrSignUp ? 'ActiveLogin' : 'default'}`} >Ingresar</div>
             </Link>
 
-            <Link to={`/auth/SignUp`} className="no-decoration">
-                <div className="right-block">Registro</div>
+            <Link to={`/auth/SignUp`} className={`${isRouteLoginOrSignUp ? 'no-decoration' : 'activeShadowSignUp'}`}>
+                <div className={`right-block ${isRouteLoginOrSignUp ? ' default' : 'ActiveRegister '}`}>Registro</div>
             </Link>
 
 
